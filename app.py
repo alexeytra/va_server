9065f8c613518fc29bfb1413fe45e50f9c7d1775
Алексей Трапезников <alekseytra@gmail.com>
2021-02-03 23:20:34 +0800

    add yargy and seq2seq in process_question

--- app.py
+++ app.py
@@ -6,27 +6,57 @@ from utils.audio_worker import text_to_speech, speech_to_text
 from utils.intent_processing import get_answer_from_tag
 import os
 from utils.load_data import classes, ic_model, ic_tokenizer, label_encoder, seq2seq_model, seq2seq_tokenizer
+from yargy import Parser
+from yargy.pipelines import morph_pipeline
+from datetime import date
 
 app = Flask(__name__)
 
 BASE_URL = '/va/api/v1/'
 app.config['UPLOAD_PATH'] = '/temp_data/'
 
+KEY_WORDS = ['Системы информатики', 'СИ',
+             'Электротехнический', 'ЭТФ',
+             'Строительный', 'СФ', 'Электронно-вычислительные системы',
+             'Электронно вычислительные системы', "ЭВС"]
+RULE = morph_pipeline(KEY_WORDS)
+parser = Parser(RULE)
+
 
 def process_question(data):
     response = {
-        "audio_answer": request.host_url + BASE_URL[1:] + 'audio_answer'
+        "audioAnswer": request.host_url + BASE_URL[1:] + 'audio_answer'
     }
+    question = data['question']
+    extract_entity = ''
+    if parser.find(question):
+        extract_entity = [_.value for _ in parser.find(question).tokens]
+        extract_entity = ' '.join(map(str, extract_entity))
+        question = question.replace(extract_entity, '').strip()
     intent_classifier = IntentClassifier(classes, ic_model, ic_tokenizer, label_encoder)
-    intent_tag = intent_classifier.get_intent(data['question'])
-    data = get_answer_from_tag(intent_tag)
-    if data[1] != '':
-        answer = str(data[0] + ' ' + data[1])
-        text_to_speech(data[0])
+    intent_tag = intent_classifier.get_intent(question)
+    if intent_tag == 'unrecognized_question':
+        seq2seq = Seq2SeqModel(seq2seq_model, seq2seq_tokenizer, 15)
+        answer = seq2seq.get_answer(question)
         response['answer'] = answer
-        return response
-    text_to_speech(data[0])
-    response['answer'] = data[0]
+        response['seq2seq'] = True
+        text_to_speech(answer)
+    else:
+        data = get_answer_from_tag(intent_tag)
+        if data[1] != '':
+            answer = str(data[0] + ' ' + data[1])
+            text_to_speech(data[0])
+            response['answer'] = answer
+            response['seq2seq'] = False
+            return response
+        text_to_speech(data[0])
+        response['answer'] = data[0]
+        response['seq2seq'] = False
+    response['language'] = 'ru'
+    response['intent'] = intent_tag
+    response['structInfo'] = extract_entity
+    response['dateTime'] = date.today()
+
     return response
 
 
