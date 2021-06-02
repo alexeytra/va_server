64c124060b48a52d189d4b6a1180f558bc69c90c
taa <alekceytr@mail.ru>
2021-06-02 17:39:56 +0800

    get department by intent

--- functions/index.py
+++ functions/index.py
@@ -1,7 +1,7 @@
 import random
 
 from constants.constants import ANSWERS_FOR_UNAUTHORIZED
-from functions.user import get_user_name
+from functions.user import *
 
 
 def default_response(response, user_type, user_token):
@@ -15,6 +15,14 @@ def process_my_name(response, user_type, user_token):
     return random.choice(ANSWERS_FOR_UNAUTHORIZED)
 
 
+def process_my_department(response, user_type, user_token):
+    if user_token != '':
+        name = get_user_department(user_type, user_token)
+        return response.replace('*', name)
+    return random.choice(ANSWERS_FOR_UNAUTHORIZED)
+
+
 user_intent = {
     'my_name': process_my_name,
+    'my_department': process_my_department
 }
--- functions/user.py
+++ functions/user.py
@@ -17,3 +17,19 @@ def get_user_name(user_type, user_token):
         print(f'HTTP error occurred: {http_err}')
     except Exception as err:
         print(f'Other error occurred: {err}')
+
+
+def get_user_department(user_type, user_token):
+    try:
+        if user_type == 'STUDENT':
+            response = requests.get(ESSTU_URL + 'student/getInfo',
+                                    headers={'Authorization': 'Bearer ' + user_token})
+        else:
+            response = requests.get(ESSTU_URL + 'employee/getInfo',
+                                    headers={'Authorization': 'Bearer' + user_token})
+        response.raise_for_status()
+        return response.json()['chairName']
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
