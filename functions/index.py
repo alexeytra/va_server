2030cdce6c0d6da77ea7aeea5918dd7617aca0c0
Алексей Трапезников <alekseytra@gmail.com>
2021-06-11 23:14:58 +0800

    fixes

--- functions/index.py
+++ functions/index.py
@@ -22,7 +22,14 @@ def process_my_department(response, user_type, user_token):
     return random.choice(ANSWERS_FOR_UNAUTHORIZED)
 
 
+def process_my_curriculum(response, user_type, user_token):
+    if user_token != '':
+        return response
+    return random.choice(ANSWERS_FOR_UNAUTHORIZED)
+
+
 user_intent = {
     'my_name': process_my_name,
-    'my_department': process_my_department
+    'my_department': process_my_department,
+    'my_curriculum': process_my_curriculum
 }
